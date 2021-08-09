a06469ad971dd264036c341af78349c6ebc26595
Samuel Cazelli <samuelcazelli@gmail.com>
2021-08-09 08:10:41 -0300

    Tests on main component

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Card } from './shared/models/cards.model';
+import { CardsService } from './shared/services/cards.service';
+import { CachetaStore } from './shared/state/store/root.store';
+import * as gameActions from './shared/state/actions/game.actions';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cacheta-game';
+
+  deck?: Card[];
+
+  constructor(
+    public cardsService: CardsService,
+    public store: Store<CachetaStore>
+  ) { }
+
+  ngOnInit() {
+    this.deck = this.cardsService.getDeck();
+  }
+
+  startGame(){
+    this.store.dispatch(gameActions.startGame());
+  }
+
 }
